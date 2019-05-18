d59e3f1d9bead0fd585df6fedfca171cbc184d46
Max Sun <max.sun@yale.edu>
2019-05-18 06:49:01 +0000

    Automatically backed up by Learn

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -1,5 +1,10 @@
 def get_first_name_of_season_winner(data, season)
-  # code here
+  data[season].each do |contestant|
+    if contestant["status"] == "Winner"
+      array = contestant["name"].split(" ")
+      return array[0]
+    end
+  end
 end
 
 def get_contestant_name(data, occupation)
