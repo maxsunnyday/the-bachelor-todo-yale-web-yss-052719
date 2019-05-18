b108c22a5fd4f061b5de124814b0af186afb3215
Max Sun <max.sun@yale.edu>
2019-05-18 07:08:56 +0000

    Bachelor

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -1,19 +1,52 @@
+require "pry"
 def get_first_name_of_season_winner(data, season)
-  # code here
+  data[season].each do |contestant|
+    if contestant["status"] == "Winner"
+      array = contestant["name"].split(" ")
+      return array[0]
+    end
+  end
 end
 
 def get_contestant_name(data, occupation)
-  # code here
+  data.each do |season, array|
+    array.each do |contestant|
+      if contestant["occupation"] == occupation
+        return contestant["name"]
+      end
+    end
+  end
 end
 
 def count_contestants_by_hometown(data, hometown)
-  # code here
+  hometown_counter = 0
+  data.each do |season, array|
+    array.each do |contestant|
+      if contestant["hometown"] == hometown
+        hometown_counter += 1
+      end
+    end
+  end
+  hometown_counter
 end
 
 def get_occupation(data, hometown)
-  # code here
+  data.each do |season, array|
+    array.each do |contestant|
+      if contestant["hometown"] == hometown
+        return contestant["occupation"]
+        break
+      end
+    end
+  end
 end
 
 def get_average_age_for_season(data, season)
-  # code here
+  sum_ages = 0
+  data[season].each do |hash|
+    sum_ages = sum_ages + hash["age"].to_f
+  end
+  number = data[season].length
+  average_age = sum_ages / number
+  average_age.round
 end
